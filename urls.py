6acfac629c4680a517c2ff7ae33fac5271b7f4b4
Hojibek <hojibek220089@gmail.com>
2025-09-29 11:58:46 +0500

    first commit

--- urls.py
+++ urls.py
@@ -2,11 +2,19 @@
 from django.urls import path
 
 # from config.urls import urlpatterns
-from .views import shahar_uchun, viloyat_uchun, malakat_uchun, info_beruvchi
+from .views import shahar_uchun, viloyat_uchun, malakat_uchun, info_beruvchi, all_news, detail, YangiliklarList
 
 urlpatterns = [
+    path('yangiliklar_list/', YangiliklarList.as_view(), name='yangiliklar-list'),
     path('shahar_uchun/', shahar_uchun),
     path('viloyat_uchun/', viloyat_uchun),
     path('malakat_uchun/', malakat_uchun),
-    path('info_beruvchi/', info_beruvchi)
+    path('info_beruvchi/', info_beruvchi),
+    path('all_news/', all_news, name='news'),
+    path('detail/<int:id>/', detail, name='detail'),
+
+
 ]
+# urlpatterns += [
+#     path('yangiliklar-list/', YangiliklarList.as_view(), name='yangiliklar-list')
+# ]
--- views.py
+++ views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 
 from django.http import HttpResponse
+from .models import Yangiliklar
+from rest_framework import generics
+from .serializers import YangiliklarSerializer
 
 def shahar_uchun(request):
     return HttpResponse("Termiz shahar")
@@ -16,3 +19,21 @@ def info_beruvchi(request):
     text = "Ism: Xojibek\nFamiliya: Amonturdiyev\nTel: +998952767642"
     return HttpResponse(text)
 
+def all_news(request):
+    news = Yangiliklar.objects.all()
+    context = {
+        'news':news
+    }
+    return render(request, 'all_news.html', context)
+
+
+def detail(request, id):
+    yangilik = Yangiliklar.objects.get(id=id)
+    context = {
+        'yangilik':yangilik
+    }
+    return render(request, 'detail.html', context)
+
+class YangiliklarList(generics.ListAPIView):
+    queryset = Yangiliklar.objects.all()
+    serializer_class = YangiliklarSerializer
